aa56b0b090d17f78e6d3bfba7a6af15691100324
Sonali-Nilesh <115540057+Sonali-Nilesh@users.noreply.github.com>
2022-11-27 19:37:45 +0100

    Update tshirts_test.cpp

--- tshirts_test.cpp
+++ tshirts_test.cpp
@@ -5,12 +5,10 @@
 
 void tshirts_test()
 {
+    std::cout << "Test execution\n";
     assert(size(37) == 'S');
     assert(size(40) == 'M');
     assert(size(43) == 'L');
     assert(size(38) == 'M');
     assert(size(42) == 'M');
-    std::cout << "Hi\n";
-    return 0;
-   
 }
