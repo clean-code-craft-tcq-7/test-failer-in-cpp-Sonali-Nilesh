d4d4e11ea56f5dd244ebaaf3bcd8ee92c2bbcc6f
Sonali-Nilesh <115540057+Sonali-Nilesh@users.noreply.github.com>
2022-11-27 19:20:52 +0100

    Update tshirts_test.cpp

--- tshirts_test.cpp
+++ tshirts_test.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <assert.h>
 #include "tshirts.hpp"
 #include "tshirts.cpp"
@@ -9,5 +10,6 @@ void tshirts_test()
     assert(size(43) == 'L');
     assert(size(38) == 'M');
     assert(size(42) == 'M');
+    std::cout << "Hi\n";
    
 }
