bb6e73fb88f8731d6aabb7f3ee7521eed9c3ba0c
Sonali-Nilesh <115540057+Sonali-Nilesh@users.noreply.github.com>
2022-11-28 15:47:54 +0100

    Update misaligned.hpp

--- misaligned.hpp
+++ misaligned.hpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
-int printfirstmajorcolor(int, int, int);
-int printfirstmajorcolor(int, int, int);
-int printsecondmajorcolor(int, int, int);
-int printthirdmajorcolor(int, int, int);
-int printfourthmajorcolor(int, int, int);
-int printfifthmajorcolor(int, int, int);
+char *printfirstmajorcolor(int, int, int);
+char *printfirstmajorcolor(int, int, int);
+char *printsecondmajorcolor(int, int, int);
+char *printthirdmajorcolor(int, int, int);
+char *printfourthmajorcolor(int, int, int);
+char *printfifthmajorcolor(int, int, int);
 int inc, first, second, third, fourth, fifth =0, n= 5;
