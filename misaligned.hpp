b52ac309298cd6d249ab7894d4851d2a47e6400e
Sonali-Nilesh <115540057+Sonali-Nilesh@users.noreply.github.com>
2022-11-28 15:55:34 +0100

    Update misaligned.hpp

--- misaligned.hpp
+++ misaligned.hpp
@@ -6,4 +6,3 @@ char *printsecondmajorcolor(int, int, int);
 char *printthirdmajorcolor(int, int, int);
 char *printfourthmajorcolor(int, int, int);
 char *printfifthmajorcolor(int, int, int);
-int inc, first, second, third, fourth, fifth =0, n= 5;
