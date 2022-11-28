2133b5a9567ee0bafc86bf383cc3d37b7cd73786
Sonali-Nilesh <115540057+Sonali-Nilesh@users.noreply.github.com>
2022-11-28 15:39:57 +0100

    Update test_misaligned.hpp

--- test_misaligned.hpp
+++ test_misaligned.hpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <assert.h>
+#include "misaligned.cpp"
+#include "misaligned.hpp"
+#include "printminorcolors.cpp"
 
 // printfirstmajorcolor(majorcolor, minorcolor, nof_maj_color)
     assert(printfirstmajorcolor(2,4,5) == "Brown"); //test minor color
