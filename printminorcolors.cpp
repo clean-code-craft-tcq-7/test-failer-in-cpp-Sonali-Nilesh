6c080fdb7924d6a2f8b65aee0e8cb3701290153a
Sonali-Nilesh <115540057+Sonali-Nilesh@users.noreply.github.com>
2022-11-29 08:02:48 +0100

    Update printminorcolors.cpp

--- printminorcolors.cpp
+++ printminorcolors.cpp
@@ -1,6 +1,6 @@
 #include "misaligned.hpp"
 
-char printfirstmajorcolor(int i, int first, int n){
+char *printfirstmajorcolor(int i, int first, int n){
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
 	
@@ -13,8 +13,7 @@ char *printsecondmajorcolor(int i, int second, int n){
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
     
     std::cout << i * n + second << " " " | " << majorColor[i] << " " " " " " " | " << minorColor[i] << "\n";
-	const char* check = minorColor[i];
-	return (char *)check;
+	return minorColor[i];
 }
 
 char *printthirdmajorcolor(int i, int third, int n){
@@ -22,8 +21,7 @@ char *printthirdmajorcolor(int i, int third, int n){
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
     
     std::cout << i * n + third << " | " << majorColor[i] << " " " | " << minorColor[i] << "\n";
-    const char* check = minorColor[i];
-    return (char *)check;
+    return minorColor[i];
 }
 
 char *printfourthmajorcolor(int i, int fourth, int n){
@@ -31,8 +29,7 @@ char *printfourthmajorcolor(int i, int fourth, int n){
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
     
     std::cout << i * n + fourth << " | " << majorColor[i] << " | " << minorColor[i] << "\n";
-    const char* check = minorColor[i];
-    return (char *)check;
+    return minorColor[i];
 }
 
 char *printfifthmajorcolor(int i, int fifth, int n){
@@ -40,6 +37,5 @@ char *printfifthmajorcolor(int i, int fifth, int n){
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
     
     std::cout << i * n + fifth << " | " << majorColor[i] <<  " | " << minorColor[i] << "\n";
-    const char* check = minorColor[i];
-    return (char *)check;
+    return minorColor[i];
 }
