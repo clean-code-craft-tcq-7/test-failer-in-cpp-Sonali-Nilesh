77637e6a03c48f6375ff7c44f43679dc9b5a3445
Sonali-Nilesh <115540057+Sonali-Nilesh@users.noreply.github.com>
2022-11-29 08:45:51 +0100

    Update test_misaligned.hpp

--- test_misaligned.hpp
+++ test_misaligned.hpp
@@ -8,9 +8,12 @@ void test_minor_color()
 {
 
 int check_minor[NOF_MINOR];
-check_minor[0]= strcmp(printfirstmajorcolor(1,4,5), "Slate"); 
-
+  
+check_minor[0]= strcmp(printfirstmajorcolor(0,0,5), "Blue"); 
 assert(check_minor[0] ==1);
+  
+check_minor[1]= strcmp(printfirstmajorcolor(1,4,5), "Slate"); 
+assert(check_minor[1] ==1);
 
 std::cout << "All is not well (maybe!)\n";
 }
