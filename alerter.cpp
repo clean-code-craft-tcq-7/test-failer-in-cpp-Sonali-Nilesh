08cd78620a5d16a99a268353e7b6057dc3dc77a5
Sonali-Nilesh <115540057+Sonali-Nilesh@users.noreply.github.com>
2022-12-06 12:58:32 +0530

    Update alerter.cpp

--- alerter.cpp
+++ alerter.cpp
@@ -1,41 +1,36 @@
 #include <iostream>
 #include <assert.h>
 #include "alerter.hpp"
+#include "alerter_stub.cpp"
 
+#define CEL_OKTHR 200
+#define CEL_NOKTHR 500
 
+#define SWENV 0
+    // if production then set to 1, if testing then set to 0
 
 int alertFailureCount = 0;
 
-int networkAlertStub(float celcius) {
-    std::cout << "ALERT: Temperature is " << celcius << " celcius.\n";
-    // Return 200 for ok
-    // Return 500 for not-ok
-    // stub always succeeds and returns 200
-    if (celcius <= 200){
-    return 200;
-    }
-    else 
-    return 500;
-}
-
 void alertInCelcius(float farenheit) {
     float celcius = (farenheit - 32) * 5 / 9;
-    int returnCode = networkAlertStub(celcius);
-    if (returnCode != 200) {
-        // non-ok response is not an error! Issues happen in life!
-        // let us keep a count of failures to report
-        // However, this code doesn't count failures!
-        // Add a test below to catch this bug. Alter the stub above, if needed.
-        alertFailureCount += 0;
-    }
+    
+        if(!SWENV){             //testing environment
+            int returnCode = networkAlertStub(celcius);
+            if (returnCode != CEL_OKTHR) {
+                // non-ok response is not an error! Issues happen in life!
+                // let us keep a count of failures to report
+                // However, this code doesn't count failures!
+                // Add a test below to catch this bug. Alter the stub above, if needed.
+                alertFailureCount += 0;
+                }
+             alerttest(farenheit, alertFailureCount);
+            }
 }
 
 int main() {
     alertInCelcius(400.5);
-        alerttest(alertFailureCount);
     alertInCelcius(303.6);
-        alerttest(alertFailureCount);
-    std::cout << alertFailureCount << " alerts failed.\n";
+   
     std::cout << "All is well (maybe!)\n";
     return 0;
 }
